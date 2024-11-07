604aef674dfaeb0b269ee04d9383340e5d7485b2
Richard <richardbalke@gmail.com>
2024-11-07 13:53:59 +0100

    Layout pagina + Global CSS style

---
+++ JavaScript/script.js
@@ -0,0 +1,8 @@
+function includeHTML(source, dst) {
+  fetch(source)
+    .then((response) => response.text())
+    .then((data) => {
+      document.querySelector(dst).innerHTML = data;
+    })
+    .catch((error) => console.error("Error fetching the menu: ", error));
+}
